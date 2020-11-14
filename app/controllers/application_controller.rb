9d31d6c0b0da7d0143a7ebd2be397cb999686115
Brenda Ferrufino <ferrufinob@gmail.com>
2020-11-14 18:36:17 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,48 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+#CREATE
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+     @article =Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #READ
+  get '/articles' do
+    #grab all articles and store them in @articles
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    #show an individual article
+    #use AR to grab the article with the id that is in 
+    #the params and set it equal to @article
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #UPDATE
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #DELETE
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect to "/articles"
+  end
+
 end
